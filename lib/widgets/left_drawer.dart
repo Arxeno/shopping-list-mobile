f5d26bdb2311d0c0dada8f0f65b964bdb7a0c4aa
Arxeno <rmfbkw04@gmail.com>
2023-11-14 16:21:49 +0700

    remove duplicate import

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:shopping_list/screens/menu.dart';
 // TODO: Impor halaman ShopFormPage jika sudah dibuat
 import 'package:shopping_list/screens/shoplist_form.dart';
